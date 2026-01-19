e03daa1445761bff825e1950df73802d12bceefa
Lyyn <vladlyyn@gmail.com>
2026-01-19 15:57:27 +0300

    Созданы 2 автотеста с фиксутрой. Из-за периодического падения теста принято решение увеличить таймаут до 10.

---
+++ just_file.py
@@ -0,0 +1,55 @@
+l = list("first")
+print(l)
+
+print("--------")
+
+import math
+print(math.pi)
+print(round(13.53287, 2))
+
+print('"Hello \"\"\"\'\' \n World"')
+print("-------")
+s = "Hello, \nworld!"
+print(s)
+
+s = "Hello, \\n world!"
+print(s)
+
+s = r"Hello, \n world!"
+print(s)
+
+s = r"Hello, \n'''' world!"
+print(s)
+
+email = "user@domain.com"
+print(email[0])
+
+print(email[-1])
+
+print(email[0:5])
+
+print(email[0:10:2])
+
+assert email.endswith("@domain.com")
+print(email.split("@"))
+
+print(email.
+      split("@"))
+
+a = "Hello" " Hello"
+b = "world"
+
+print(a + b)
+print(a + ", " + b + "!")
+
+print("{a}, {b}!".format(a=a, b=b))
+print(f"{a}, {b}!")
+
+
+
+
+
+
+
+
+
